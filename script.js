e3121a9fafa34a11ec322705b8b94e6f51eb69c4
Giemy Vargas <ma_cho@hotmail.com>
2022-05-06 20:04:14 -0500

    madrev2

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const enviarNombre = (e)=>{
     $span.textContent=`${$name.value}`;
     setInterval(()=>{
         document.querySelector("h5").classList.add("hidden");
-    },8000);
+    },7000);
 
 
 }
